a2df82329d1375e3038cccfc033838427154a395
Trevor O'Dwyer <trevorodwyer619@hotmail.com>
2020-08-03 16:21:03 +0100

    add call to quote api im app.js file

---
+++ app.js
@@ -0,0 +1,18 @@
+// Get quote from api
+async function getQuote() {
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+    const apiUrl = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    try {
+        const response = await fetch(proxyUrl + apiUrl);
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        getQuote()
+        console.log('No quote', error);
+
+    }
+
+}
+
+// On load 
+getQuote();
