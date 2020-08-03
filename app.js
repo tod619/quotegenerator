8f9daf55e9ddb31c9df625a76cb2fdcac3bc98bd
Trevor O'Dwyer <trevorodwyer619@hotmail.com>
2020-08-03 17:25:16 +0100

    Add dom variables to app.js file

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
+// Variables
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 // Get quote from api
 async function getQuote() {
-    const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
-    const apiUrl = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
-    try {
-        const response = await fetch(proxyUrl + apiUrl);
-        const data = await response.json();
-        console.log(data);
-    } catch (error) {
-        getQuote()
-        console.log('No quote', error);
-
-    }
-
+	const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+	const apiUrl = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+	try {
+		const response = await fetch(proxyUrl + apiUrl);
+		const data = await response.json();
+		console.log(data);
+	} catch (error) {
+		getQuote();
+		console.log('No quote', error);
+	}
 }
 
-// On load 
+// On load
 getQuote();
